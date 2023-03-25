b5638998b611299f6563ffdc2c05b0ecf3b22bc4
Aman Jain <jaman207168@gmail.com.com>
2023-03-25 15:45:47 +0530

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAX-U8brD6H6-oLTxrpw-xZ-IrkjXE4igU",
+  authDomain: "symiplod.firebaseapp.com",
+  projectId: "symiplod",
+  storageBucket: "symiplod.appspot.com",
+  messagingSenderId: "536374722837",
+  appId: "1:536374722837:web:dba6b74651367f0d9c691c",
+  measurementId: "G-8791P5SM4M"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
