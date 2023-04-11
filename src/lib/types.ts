fb45235c57cd63904e04daafffde93f0229fd00b
Sparsh Kaushik <github@sparshkaushik.com>
2023-04-11 23:42:03 +0530

    updated changes in api

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -5,3 +5,14 @@ export interface FileBlob {
     blob: Blob,
     base64String?: any
 }
+
+export interface SingleFetchBody {
+    isdoc1blob: boolean,
+    isdoc2blob: boolean,
+    doc1?: string,
+    doc2?: string
+    doc1blob?: string
+    doc1fileType?: string
+    doc2blob?: string
+    doc2fileType?: string
+}
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,170 +1,197 @@
 <script lang="ts">
-	import type { FileBlob } from '$lib/types';
+	import type { FileBlob, SingleFetchBody } from '$lib/types';
 	import { blobToBase64 } from '$lib/utils';
 	import './style.scss';
 
 	let doc1: string, doc2: string;
 	let doc1file: FileBlob, doc2file: FileBlob;
+    let simscore: number | null = null;
+
+	const handleCheck = () => {
+        const fbody: SingleFetchBody = {
+            isdoc1blob: false,
+            isdoc2blob: false,
+        }
+        if (doc1file) {
+            fbody.isdoc1blob = true;
+            fbody.doc1blob = doc1file.base64String
+            fbody.doc1fileType = doc1file.type
+        } else {
+            fbody.doc1 = doc1;
+        }
+        if (doc2file) {
+            fbody.isdoc2blob = true;
+            fbody.doc2blob = doc2file.base64String
+            fbody.doc2fileType = doc2file.type
+        } else {
+            fbody.doc2 = doc2;
+        }
+		fetch('http://127.0.0.1:5000/similarity', {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(fbody)
+		})
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+                console.log(data);
+                simscore = data.similarity_score;
+			});
+	};
+
+    $: console.log(simscore);
 </script>
 
 <div class="w-full flex flex-col px-8 py-8 items-center gap-y-6">
-	<div class="text-2xl font-semibold">Similarity Checker</div>
-	<div class="w-full grid dashmain gap-x-8">
-		<div class="w-full flex flex-col items-center gap-y-4">
-			{#if !doc1file}
-				<div class="w-full flex flex-col">
-					<textarea
-						bind:value={doc1}
-						class="w-full h-64 p-4 border border-gray-300 rounded-lg"
-						placeholder="Paste your text here"
-					/>
-					<!-- svelte-ignore a11y-click-events-have-key-events -->
-					<span
-						on:click={() => {
-							doc1 = '';
-						}}
-						class="ml-1 text-sm text-gray-500 cursor-pointer">Clear</span
-					>
-				</div>
-				<span class="text-lg font-semibold">OR</span>
-				<button
-					class="upload-btn"
-					on:click={() => {
-						document.getElementById('doc1file')?.click();
-					}}
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="24"
-						height="24"
-						viewBox="0 0 24 24"
-						fill="none"
-						stroke-width="2"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						><path d="M3 15v4c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2v-4M17 8l-5-5-5 5M12 4.2v10.3" /></svg
-					>
-					Upload a File
-				</button>
-				<input
-					type="file"
-					accept=".txt, .pdf, .doc, .docx"
-					class="hidden"
-					id="doc1file"
-					on:input={(e) => {
-						// make it blob
-						// @ts-ignore
-						const file = e.target.files[0];
-						const blob = new Blob([file], { type: file.type });
-						doc1file = {
-							name: file.name,
-							type: file.type,
-							size: file.size,
-							blob
-						};
-                        blobToBase64(doc1file.blob)
-							.then((base64String) => {
-								doc1file.base64String = base64String;
-							})
-							.catch((error) => {
-								// Handle the error
-								console.error(error);
-							});
-					}}
-				/>
-			{:else}
-				<div>{doc1file.name}</div>
-			{/if}
-		</div>
-		<div class="w-full flex flex-col items-center gap-y-4">
-			{#if !doc2file}
-				<div class="w-full flex flex-col">
-					<textarea
-						bind:value={doc2}
-						class="w-full h-64 p-4 border border-gray-300 rounded-lg"
-						placeholder="Paste your text here"
-					/>
-					<!-- svelte-ignore a11y-click-events-have-key-events -->
-					<span
-						on:click={() => {
-							doc2 = '';
-						}}
-						class="ml-1 text-sm text-gray-500 cursor-pointer">Clear</span
-					>
-				</div>
-				<span class="text-lg font-semibold">OR</span>
-				<button
-					class="upload-btn"
-					on:click={() => {
-						document.getElementById('doc2file')?.click();
-					}}
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="24"
-						height="24"
-						viewBox="0 0 24 24"
-						fill="none"
-						class=""
-						stroke-width="2"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						><path d="M3 15v4c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2v-4M17 8l-5-5-5 5M12 4.2v10.3" /></svg
-					>
-					Upload a File
-				</button>
-				<input
-					type="file"
-					accept=".txt, .pdf, .doc, .docx"
-					class="hidden"
-					id="doc2file"
-					on:input={(e) => {
-						// make it blob
-						// @ts-ignore
-						const file = e.target?.files[0];
-						const blob = new Blob([file], { type: file.type });
-						doc2file = {
-							name: file.name,
-							type: file.type,
-							size: file.size,
-							blob
-						};
-						blobToBase64(doc2file.blob)
-							.then((base64String) => {
-								doc2file.base64String = base64String;
-							})
-							.catch((error) => {
-								// Handle the error
-								console.error(error);
-							});
-					}}
-				/>
-			{:else}
-				<div>{doc2file.name}</div>
-			{/if}
-		</div>
-	</div>
-	<button
-		class="flex gap-4 justify-center items-center w-fit px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-indigo-500 rounded-lg hover:bg-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-50"
-		on:click={() => {
-			fetch('http://127.0.0.1:5000/similarity', {
-				method: 'POST',
-				headers: {
-					Accept: 'application/json',
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify({
-					doc1blob: doc1file.base64String,
-					doc2blob: doc2file.base64String,
-					fileType: 'txt'
-				})
-			}).then(response => {
-                response.json()
-            }).then(data => {
-                console.log(data)
-            })
-		}}
-	>
-		Check Similarity
-	</button>
+    {#if simscore == null}
+        <div class="text-2xl font-semibold">Similarity Checker</div>
+        <div class="w-full grid dashmain gap-x-8">
+            <div class="w-full flex flex-col items-center gap-y-4">
+                {#if !doc1file}
+                    <div class="w-full flex flex-col">
+                        <textarea
+                            bind:value={doc1}
+                            class="w-full h-64 p-4 border border-gray-300 rounded-lg"
+                            placeholder="Paste your text here"
+                        />
+                        <!-- svelte-ignore a11y-click-events-have-key-events -->
+                        <span
+                            on:click={() => {
+                                doc1 = '';
+                            }}
+                            class="ml-1 text-sm text-gray-500 cursor-pointer">Clear</span
+                        >
+                    </div>
+                    <span class="text-lg font-semibold">OR</span>
+                    <button
+                        class="upload-btn"
+                        on:click={() => {
+                            document.getElementById('doc1file')?.click();
+                        }}
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            ><path d="M3 15v4c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2v-4M17 8l-5-5-5 5M12 4.2v10.3" /></svg
+                        >
+                        Upload a File
+                    </button>
+                    <input
+                        type="file"
+                        accept=".txt, .pdf, .doc, .docx"
+                        class="hidden"
+                        id="doc1file"
+                        on:input={(e) => {
+                            // make it blob
+                            // @ts-ignore
+                            const file = e.target.files[0];
+                            const blob = new Blob([file], { type: file.type });
+                            doc1file = {
+                                name: file.name,
+                                type: file.type,
+                                size: file.size,
+                                blob
+                            };
+                            blobToBase64(doc1file.blob)
+                                .then((base64String) => {
+                                    doc1file.base64String = base64String;
+                                })
+                                .catch((error) => {
+                                    // Handle the error
+                                    console.error(error);
+                                });
+                        }}
+                    />
+                {:else}
+                    <div>{doc1file.name}</div>
+                {/if}
+            </div>
+            <div class="w-full flex flex-col items-center gap-y-4">
+                {#if !doc2file}
+                    <div class="w-full flex flex-col">
+                        <textarea
+                            bind:value={doc2}
+                            class="w-full h-64 p-4 border border-gray-300 rounded-lg"
+                            placeholder="Paste your text here"
+                        />
+                        <!-- svelte-ignore a11y-click-events-have-key-events -->
+                        <span
+                            on:click={() => {
+                                doc2 = '';
+                            }}
+                            class="ml-1 text-sm text-gray-500 cursor-pointer">Clear</span
+                        >
+                    </div>
+                    <span class="text-lg font-semibold">OR</span>
+                    <button
+                        class="upload-btn"
+                        on:click={() => {
+                            document.getElementById('doc2file')?.click();
+                        }}
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            class=""
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            ><path d="M3 15v4c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2v-4M17 8l-5-5-5 5M12 4.2v10.3" /></svg
+                        >
+                        Upload a File
+                    </button>
+                    <input
+                        type="file"
+                        accept=".txt, .pdf, .doc, .docx"
+                        class="hidden"
+                        id="doc2file"
+                        on:input={(e) => {
+                            // make it blob
+                            // @ts-ignore
+                            const file = e.target?.files[0];
+                            const blob = new Blob([file], { type: file.type });
+                            doc2file = {
+                                name: file.name,
+                                type: file.type,
+                                size: file.size,
+                                blob
+                            };
+                            blobToBase64(doc2file.blob)
+                                .then((base64String) => {
+                                    doc2file.base64String = base64String;
+                                })
+                                .catch((error) => {
+                                    // Handle the error
+                                    console.error(error);
+                                });
+                        }}
+                    />
+                {:else}
+                    <div>{doc2file.name}</div>
+                {/if}
+            </div>
+        </div>
+        <button
+            class="flex gap-4 justify-center items-center w-fit px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-indigo-500 rounded-lg hover:bg-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-50"
+            on:click={handleCheck}
+        >
+            Check Similarity
+        </button>
+    {:else}
+        <div class="text-2xl font-semibold">Similarity Score</div>
+        <span class="text-xl font-medium">{simscore}</span>
+    {/if}
 </div>
