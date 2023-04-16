0cc7db462569e973160686b078110b463ba3d5ee
Sparsh Kaushik <contact@sparshkaushik.com>
2023-04-16 12:42:20 +0530

    added log out method

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
-	import { authStore, isLoading } from '$lib/stores';
+	import { authHandlers, authStore, isLoading } from '$lib/stores';
 	import { isLeftBarCollapsed } from '../../lib/stores';
 	import './layoutstyle.scss';
 	import { onMount } from 'svelte';
@@ -83,13 +83,13 @@
 						<svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
 						<span class="mx-4 font-medium">Dashboard</span>
 					</div>
-					<div class="route {$page.route.id === "/settings" && "active"}"  on:click={()=>{goto("/dashboard/settings")}} on:keydown={(e)=>{e.key === "Enter" && goto("/dashboard/settings")}}>
+					<div class="route {$page.route.id === "/settings" && "active"}" on:click={()=>{goto("/dashboard/settings")}} on:keydown={(e)=>{e.key === "Enter" && goto("/dashboard/settings")}}>
 						<svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="3"></circle><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path></svg>
 						<span class="mx-4 font-medium">Settings</span>
 					</div>
 				</div>
 				<div class="flex flex-col gap-2">
-					<div class="route">
+					<div class="route" on:click={()=>{authHandlers.logout()}} on:keydown={(e)=>{e.key === "Enter" && authHandlers.logout()}}>
 						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M10 3H6a2 2 0 0 0-2 2v14c0 1.1.9 2 2 2h4M16 17l5-5-5-5M19.8 12H9"/></svg>
 						<span class="mx-4 font-medium">Log out</span>
 					</div>
